e7497fc0bfde48424b04df8a49b6c47f70a8f244
Paul Rathgeb <paul.rathgeb@skynet.be>
2016-03-04 21:15:19 +0100

    Renamed 'lib' to 'hanover_flipdot' Fixed the import in clock.py

--- clock.py
+++ clock.py
@@ -1,8 +1,8 @@
 from fonts import font
-import lib.display
+from hanover_flipdot import display
 import time
 
-display = lib.display.Display("/dev/tty.wchusbserial1d1140", font.unscii_fantasy, True)
+display = display.Display("/dev/tty.wchusbserial1d1140", font.unscii_fantasy, True)
 
 while True:
     display.write_first_line(time.strftime("%H:%M:%S"), column = 4)
