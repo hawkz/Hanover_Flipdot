6e85e9d39329592379bb31a18c90249d1f32e1b7
Steve Hawkes <steve@blanc.ltd.uk>
2016-05-28 14:28:08 +0100

    Update clock for our device

--- clock.py
+++ clock.py
@@ -2,11 +2,13 @@ from hanover_flipdot import display
 from hanover_flipdot import fonts
 import time
 
-display = display.Display("/dev/tty.wchusbserial1a1230", 5, 144, 19, fonts.unscii_mcr, True, False)
+display = display.Display("/dev/ttyUSB0", 1, 96, 16, fonts.unscii_mcr, True, False)
 
 while True:
-    display.write_text(time.strftime("%H:%M:%S"), line = 3, column = 40)
-    display.write_text(time.strftime("%a %d %b %Y"), line = 11, column = 8)
+    display.erase_all()
     display.send()
-    time.sleep(.3)
+    display.write_text(time.strftime("%H:%M:%S"), line = 1, column = 1)
+#    display.write_text(time.strftime("%a %d %b %Y"), line = 11, column = 8)
+    display.send()
+    time.sleep(3)
 
