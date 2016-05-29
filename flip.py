f246789ecb0a07e76291eb92b38613a1c912ce73
Steve Hawkes <steve@blanc.ltd.uk>
2016-05-29 09:33:42 +0100

    Add all the bits needed for BMO

--- flip.py
+++ flip.py
@@ -2,49 +2,123 @@
 # -*- coding: utf-8 -*-
 
 import os
-import time
 import numpy as np
+import time
 from glyphs import five, ten
 
-
-show_clock = True
-flippy = np.zeros((96, 16), dtype=np.int)
+np.set_printoptions(threshold='nan', linewidth=200)
+MAXWIDTH = 96
+MAXHEIGHT = 16
+flippy = np.zeros((MAXHEIGHT, MAXWIDTH), dtype=np.int)
 os.environ['TZ'] = 'Europe/London'
 
 
-def glify(text, font=ten):
-    if font == ten:
-        output = np.array([[], [], [], [], [], [], [], [], [], []], dtype=np.int)
-    elif font == five:
-        output = np.array([[], [], [], [], []], dtype=np.int)
+def erase(flippy):
+    """ Reset display to zeros. """
+    flippy = np.zeros((MAXHEIGHT, MAXWIDTH), dtype=np.int)
+    return flippy
+
+
+def glify(text, font=five):
+    output = np.array([])
 
     for char in text.replace("", " ")[1:-1]:
-        output = np.concatenate((output, np.array(font[char])), axis=1)
+        if output.size:
+            output = np.hstack((output, np.array(font[char])))
+        else:
+            output = np.array(font[char])
 
     return output
 
 
 def tiny(text):
     """ Set text in 5px font. """
-    dots = glify(text, five)
+    dots = glify(text.upper().strip(), five)
     return dots
 
 
 def huge(text):
     """ Set text in 10px font. """
-    dots = glify(text, ten)
+    dots = glify(text.upper().strip(), ten)
     return dots
 
 
-def place(dots, pos):
-    """ Place dots on display at pos e.g. (2,3) or (-1,3) for right aligned. """
-    dots = np.array(dots)
+def place(flippy, dots, pos, reverse=False):
+    """ Place dots on display at pos e.g. (2,3). Negative indices allowed. """
     x = pos[0]
     y = pos[1]
-    flippy[y:y+dots.shape[0],
-           x:x+dots.shape[1]] = dots
+    startx = 0
+    starty = 0
+
+    if reverse:
+        if x < 0:
+            x = flippy.shape[1] - dots.shape[1]
+        if y < 0:
+            y = flippy.shape[0] - dots.shape[0]
+
+        dots = dots[MAXHEIGHT * -1 - y:, MAXWIDTH * -1 - x:]
+
+        # place on flippy
+        flippy[y:y + dots.shape[0],
+               x:x + dots.shape[1]] = dots
+
+    else:
+
+        if x < 0:
+            startx = -1 * x
+            x = 0
+
+        if y < 0:
+            starty = -1 * y
+            y = 0
+
+        # crop to fit
+        dots = dots[starty:, startx:]
+        dots = dots[0:MAXHEIGHT-y, 0:MAXWIDTH-x]
+
+        # place on flippy
+        flippy[y:y + dots.shape[0],
+               x:x + dots.shape[1]] = dots
+
+    return flippy
+
+
+def fliplr(a):
+    """ Flip array left to right. """
+    return np.fliplr(a)
+
+
+def flipud(a):
+    """ Flip array top to bottom. """
+    return np.flipud(a)
 
 
 def clock():
     """ Output the dots for time. """
-    return glify(time.strftime("%l:%M").strip())
+    return glify(time.strftime(" %l:%M"))
+
+
+def convert(flippy):
+    """ Convert to Martin's format. """
+    for char in ('[ ]'):
+        flippy = str(flippy).replace(char, '')
+    flippy = flippy.replace('0', '-').replace('1', '#')
+    return flippy
+
+
+if __name__ == '__main__':
+    import display_refactored
+
+    x = MAXWIDTH
+    while True:
+        flippy = erase(flippy)
+        flippy = place(flippy, tiny('hello from bmo'), (0, 0))
+        flippy = place(flippy, huge('Who wants to play video games? ABCDEFGHIJKLMNOPQRSTUVWXYZ 01234567890'), (x, 6))
+        flippy = place(flippy, clock(), (-1, 0), True)
+        #print(convert(flippy))
+        time.sleep(0.3)
+        x -= 1
+        if x == -1 * (huge('Who wants to play video games? ABCDEFGHIJKLMNOPQRSTUVWXYZ 01234567890').shape[1]):
+            x = MAXWIDTH
+
+        display_refactored.main(convert(flippy))
